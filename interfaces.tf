fc01cfcb456a7ce86492af9ca5254f0f6c06fdc2
Ben Newton <istairbn@users.noreply.github.com>
2018-01-11 16:14:36 +0000

    Create interfaces.tf

---
+++ interfaces.tf
@@ -0,0 +1,7 @@
+variable "datadog_external_id" {
+  description = "The External ID provided for the AWS Integration."
+}
+
+variable "datadog_aws_account_id" {
+  description = "The Datadog ASWS Account Number"
+}
